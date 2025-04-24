e3fb656213f3078d8d92d5185929f6b049c89917
joaopedromsantos <joaomartinscrf2019@gmail.com>
2025-04-24 18:31:11 -0300

    :wrench: chore: Novo local da URI

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://users_owner:npg_Jjy36OlhZPHs@ep-empty-base-a4r182hx-pooler.us-east-1.aws.neon.tech/users?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
