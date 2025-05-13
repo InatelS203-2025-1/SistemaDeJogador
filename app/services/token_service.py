ef62092af730c2d93511b2dcf035fad515e7ecbc
henrique-pizzoni <rikepizzoni@gmail.com>
2025-05-13 18:11:08 -0300

    :sparkles: Feat: get details from user

--- app/services/token_service.py
+++ app/services/token_service.py
@@ -10,3 +10,14 @@ def generate_token(user_id):
     }
     token = jwt.encode(payload, current_app.config['SECRET_KEY'], algorithm=current_app.config['JWT_ALGORITHM'])
     return token
+
+
+def token_validation(auth_header, secret_key, jwt_algorithm):
+    try:
+        token = auth_header.split(" ")[1]
+        decoded = jwt.decode(token, secret_key, algorithms=jwt_algorithm)
+        user_id = decoded["user_id"]
+        return user_id, None, None
+    except (IndexError, jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        return None, {"msg": "Token inválido ou expirado"}, 401
+
